2ed3e5d4f42b1442b29bac5c20fe77db95ce974f
mzulistiyan <izulistiyan@gmail,com>
2021-11-06 18:23:05 +0700

    finish all migrate

---
+++ database/migrations/2021_11_06_103459_create_camps_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCampsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('camps', function (Blueprint $table) {
+            $table->id();
+            $table->string('title',100);
+            $table->string('slug',100);
+            $table->integer('price')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('camps');
+    }
+}
