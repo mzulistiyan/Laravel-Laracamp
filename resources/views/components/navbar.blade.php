786dbfa58bb7780b8bf4af93f6e1f5cd60c77cc3
mzulistiyan <izulistiyan@gmail,com>
2021-11-07 16:38:38 +0700

    new

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -21,6 +21,26 @@
                         <a class="nav-link" href="#">Business</a>
                     </li>
                 </ul>
+                @auth
+                <div class="d-flex user-logged nav-item dropdown no-arrow">
+                    <a href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-labelledby="false">
+                        Halo, {{Auth::user()->name}}!
+                        <img src="{{Auth::user()->avatar}}" class="user-photo" alt="">
+                        <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink" style="right: 0; left:auto">
+                        <li>
+                            <a href="" class="dropdown-item">My Dashboard</a>
+                        </li>
+                        <li>
+                            <a href="" class="dropdown-item" onclick="event.preventDefault(); document.getElementById('logout-form').submit()">Sign Out</a>
+                            <form action="{{route('logout')}}" method="post" style="display: none;" id="logout-form">
+                            <input type="hidden" name="_token" value="{{csrf_token()}}">
+                        </form>
+                        </li>
+                    
+                    </ul>
+                    </a>
+                </div>
+                @else
                 <div class="d-flex">
                     <a href="{{route('login')}}" class="btn btn-master btn-secondary me-3">
                         Sign In
@@ -29,6 +49,8 @@
                         Sign Up
                     </a>
                 </div>
+                @endauth
+
             </div>
         </div>
     </nav>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\UserController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,14 +17,22 @@ Route::get('/', function () {
     return view('welcome');
 })->name('welcome');
 
-Route::get('login', function () {
-    return view('login');
-})->name('login');
-
 Route::get('checkout', function () {
     return view('checkout');
-})->name('checkout');
+})->name('checkout');;
 
 Route::get('success-checkout', function () {
     return view('success_checkout');
-})->name('success-checkout');
+})->name('success-checkout');;
+
+
+//scoalite Routes
+Route::get('sign-in-google', [UserController::class,'google'])->name('user.login.google');
+Route::get('auth/google/callback', [UserController::class,'HandleProviderCallback'])->name('user.google.callback');
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
