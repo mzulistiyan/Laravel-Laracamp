903908de2f7d742ff5963431c38a1835346fbac8
mzulistiyan <izulistiyan@gmail,com>
2021-11-07 15:17:06 +0700

    install brezee

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-light">
         <div class="container">
-            <a class="navbar-brand" href="{{route('welcome')}}">
+            <a class="navbar-brand" href="">
                 <img src="{{asset('images/logo.png')}}" alt="">
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
--- routes/web.php
+++ routes/web.php
@@ -17,14 +17,20 @@ Route::get('/', function () {
     return view('welcome');
 })->name('welcome');
 
-Route::get('login', function () {
-    return view('login');
-})->name('login');
-
 Route::get('checkout', function () {
     return view('checkout');
-})->name('checkout');
+})->name('checkout');;
 
 Route::get('success-checkout', function () {
     return view('success_checkout');
-})->name('success-checkout');
+})->name('success-checkout');;
+
+Route::get('/', function () {
+    return view('welcome');
+})->name('');;
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
